75eccb7fc0e80e68ae7756e3500bcba70bd10b64
Alexis Isidoro Bolaños Avalos <alexisisidorobolanosavalos@MacBook-Air-de-Alexis-2.local>
2021-04-16 16:10:56 -0500

    Mi primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { v4 } from 'uuid';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [myEmail, setMyEmail] = useState('');
+  const [myComment, setMyComment] = useState('');
+  const [comments, setComments] = useState([]);
+
+  const handleSubmit = ({ nativeEvent }) => {
+    nativeEvent.preventDefault();
+    if (!myComment.trim() || !myEmail.trim()) {
+      // eslint-disable-next-line
+      alert('Por favor llena los campos requeridos');
+      return;
+    }
+    setComments([
+      ...comments,
+      {
+        id: v4(),
+        email: myEmail,
+        comment: myComment,
+      },
+    ]);
+    setMyEmail('');
+    setMyComment('');
+  };
+
+  const editComment = (data) => {
+    setMyComment(data.comment);
+    setMyEmail(data.email);
+  };
+
+  const deleteComment = (id) => {
+    setComments(comments.filter((comment) => comment.id !== id));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form className="form-container">
+        <input
+          placeholder="Email"
+          type="email"
+          value={myEmail}
+          onChange={({ target }) => setMyEmail(target.value)}
+        />
+        <textarea
+          placeholder="Comment"
+          rows={5}
+          onChange={({ target }) => setMyComment(target.value)}
+          value={myComment}
+        />
+        <div className="button-container">
+          <button
+            type="submit"
+            className="submit-button"
+            onClick={handleSubmit}
+          >
+            Enviar
+          </button>
+        </div>
+      </form>
+      {comments.length > 0 && (
+        <div className="form-container">
+          {comments.map((comment) => (
+            <div key={comment.id} className="comment-container">
+              <div className="comment-fl">
+                <h3>{comment.email}</h3>
+                <div>
+                  <button type="button" onClick={() => editComment(comment)}>
+                    Edit
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => deleteComment(comment.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+              <p>{comment.comment}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
